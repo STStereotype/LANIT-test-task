8567d9cb5f2c7930825dd2592bd0eb558c54c958
STStereotype <62321778+STStereotype@users.noreply.github.com>
2022-04-18 11:44:32 +0700

    Разметка и стилизация страницы бронирования

--- src/app/modules/reservation/pages/reservation/reservation.component.ts
+++ src/app/modules/reservation/pages/reservation/reservation.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {catchError, map} from 'rxjs/operators';
+import {HotelRoomTypeModel} from '../../../shared/models/reservation.model';
+import {ReservationService} from '../../services';
 
 @Component({
     selector: 'app-reservation',
     templateUrl: './reservation.component.html'
 })
 
-export class ReservationComponent {
-    constructor() {}
+export class ReservationComponent implements OnInit  {
+
+    hotelRoomType: HotelRoomTypeModel[];
+
+    constructor(private hotelRoomTypeService: ReservationService) {}
+
+    ngOnInit(): void {
+        this.hotelRoomTypeService.loadInfo().pipe(
+            map(data => {
+                this.hotelRoomType = data;
+            }),
+            catchError(error => {
+                console.error(error);
+                return [];
+            })
+        ).subscribe();
+    }
 }
