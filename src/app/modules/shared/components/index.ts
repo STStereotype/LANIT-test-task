5ab099262b3200be151a64c7726309c92eac7752
STStereotype <62321778+STStereotype@users.noreply.github.com>
2022-04-25 19:09:44 +0700

    Удаление компонента

--- src/app/modules/shared/components/index.ts
+++ src/app/modules/shared/components/index.ts
@@ -1,14 +1,11 @@
 export * from './block-link/block-link.component';
 export * from './weather/weather.component';
-export * from './input-only-letters/input-only-letters.component';
 
 import { BlockLinkComponent } from './block-link/block-link.component';
 import { WeatherComponent } from './weather/weather.component';
-import {InputOnlyLettersComponent } from './input-only-letters/input-only-letters.component';
 
 
 export const COMPONENTS = [
     BlockLinkComponent,
-    WeatherComponent,
-    InputOnlyLettersComponent
+    WeatherComponent
 ];
